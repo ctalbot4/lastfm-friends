082c2e1d9ac0ce3b559088eddf6aa3797e7c5b3c
ctalbot2004 <ctalbot2004@gmail.com>
2026-04-26 20:16:23 -0400

    Clean up GA event params

--- src/ui/analytics.js
+++ src/ui/analytics.js
@@ -7,53 +7,53 @@ export function initAnalytics() {
         if (target.closest("#artists-list") && target.closest("a")) {
             gtag("event", "chart_click", {
                 click_type: "artists",
-                artist_target: target.textContent,
+                target_artist: target.textContent,
             });
         } else if (target.closest("#albums-list") && target.closest("a")) {
             gtag("event", "chart_click", {
                 click_type: "albums",
-                album_target: target.textContent,
+                target_album: target.textContent,
             });
         } else if (target.closest("#tracks-list") && target.closest("a")) {
             gtag("event", "chart_click", {
                 click_type: "tracks",
-                track_target: target.textContent,
+                target_track: target.textContent,
             });
         } else if (target.closest("#listeners-list") && target.closest("a")) {
             gtag("event", "chart_click", {
                 click_type: "listeners",
-                listener_target: target.textContent,
+                target_username: target.textContent,
             });
         }
         else if (target.closest("#unique-artists-list") && target.closest("a")) {
             gtag("event", "chart_click", {
                 click_type: "unique_artists",
-                artist_target: target.textContent,
+                target_artist: target.textContent,
             });
         } else if (target.closest("#unique-tracks-list") && target.closest("a")) {
             gtag("event", "chart_click", {
                 click_type: "unique_tracks",
-                track_target: target.textContent,
+                target_track: target.textContent,
             });
         } else if (target.closest("#listening-streaks-list") && target.closest("a")) {
             gtag("event", "chart_click", {
                 click_type: "listening_streaks",
-                streak_target: target.textContent,
+                target_username: target.textContent,
             });
         } else if (target.closest("#artist-streaks-list") && target.closest("a")) {
             gtag("event", "chart_click", {
                 click_type: "artist_streaks",
-                streak_target: target.textContent,
+                target_artist: target.textContent,
             });
         } else if (target.closest("#album-streaks-list") && target.closest("a")) {
             gtag("event", "chart_click", {
                 click_type: "album_streaks",
-                streak_target: target.textContent,
+                target_album: target.textContent,
             });
         } else if (target.closest("#track-streaks-list") && target.closest("a")) {
             gtag("event", "chart_click", {
                 click_type: "track_streaks",
-                streak_target: target.textContent,
+                target_track: target.textContent,
             });
         }
 
@@ -62,19 +62,28 @@ export function initAnalytics() {
             if (target.closest(".artist-title")) {
                 gtag("event", "block_click", {
                     click_type: "artist",
-                    artist_target: target.textContent,
+                    target_artist: target.textContent,
                 });
             } else if (target.closest(".song-title")) {
                 gtag("event", "block_click", {
                     click_type: "track",
-                    track_target: target.textContent,
+                    target_track: target.textContent,
                 });
             } else if (target.closest(".info-button")) {
-                gtag("event", "info_click", {});
+                const block = target.closest(".block");
+                const username = block?.dataset.username;
+                const artist = block?.querySelector(".artist-title > a").textContent.trim();
+                const track = block?.querySelector(".song-title > a").textContent.trim();
+
+                gtag("event", "info_click", {
+                    target_username: username,
+                    target_artist: artist,
+                    target_track: track,
+                });
             } else if (target.closest(".user-info")) {
                 gtag("event", "block_click", {
                     click_type: "username",
-                    user_target: target.textContent,
+                    target_username: target.textContent,
                 });
             }
         }
@@ -84,17 +93,17 @@ export function initAnalytics() {
             if (target.closest(".ticker-artist") && target.closest("a")) {
                 gtag("event", "ticker_click", {
                     click_type: "artist",
-                    artist_target: target.textContent,
+                    target_artist: target.textContent,
                 });
             } else if (target.closest(".ticker-album") && target.closest("a")) {
                 gtag("event", "ticker_click", {
                     click_type: "album",
-                    album_target: target.textContent,
+                    target_album: target.textContent,
                 });
             } else if (target.closest(".ticker-track") && target.closest("a")) {
                 gtag("event", "ticker_click", {
                     click_type: "track",
-                    track_target: target.textContent,
+                    target_track: target.textContent,
                 });
             }
         }
